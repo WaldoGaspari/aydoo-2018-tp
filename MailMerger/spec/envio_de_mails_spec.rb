f44ef4241888f722a03d098ecd9324a888574c4c
MAXIMILIANO FERNANDO BISCARDI <biscardi@ar.ibm.com>
2018-06-24 18:08:27 -0300

    modificado test de envio de mails

--- MailMerger/spec/envio_de_mails_spec.rb
+++ MailMerger/spec/envio_de_mails_spec.rb
@@ -5,34 +5,20 @@ require 'json'
 
 describe 'enviar mails' do
 
-  let(:mail) { FakeEnvioDeMails.new }
 
-  it 'se envia un mail dando los parametros' do
-    mail.enviar('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
-
-    expect(mail.mails_enviados[0]["remitente"]).to eq "remitente@test.com"
-    expect(mail.mails_enviados[0]["destinatario"]).to eq "destinatario@test.com"
-    expect(mail.mails_enviados[0]["asunto"]).to eq "Prueba"
-    expect(mail.mails_enviados[0]["cuerpo_del_mensaje"]).to eq "Mensaje de prueba"
-  end
+  let(:mandador_mail) { double(EnvioDeMails.new) }
 
   it 'se envian 2 mails dando los parametros' do
-    mail.enviar('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
-    mail.enviar('carlos@test.com', 'mabel@test.com', 'test', 'hola')
+    expect(mandador_mail).to receive(:enviar).with('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
+    expect(mandador_mail).to receive(:enviar).with('carlos@test.com', 'mabel@test.com', 'test', 'hola')
 
-    expect(mail.mails_enviados[0]["remitente"]).to eq "remitente@test.com"
-    expect(mail.mails_enviados[0]["destinatario"]).to eq "destinatario@test.com"
-    expect(mail.mails_enviados[0]["asunto"]).to eq "Prueba"
-    expect(mail.mails_enviados[0]["cuerpo_del_mensaje"]).to eq "Mensaje de prueba"
-    expect(mail.mails_enviados[1]["remitente"]).to eq "carlos@test.com"
-    expect(mail.mails_enviados[1]["destinatario"]).to eq "mabel@test.com"
-    expect(mail.mails_enviados[1]["asunto"]).to eq "test"
-    expect(mail.mails_enviados[1]["cuerpo_del_mensaje"]).to eq "hola"
+    mandador_mail.enviar('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
+    mandador_mail.enviar('carlos@test.com', 'mabel@test.com', 'test', 'hola')
   end
 
-  it 'se manda mail dando los parametros utilizando double' do
-    mandador_mail = double(EnvioDeMails.new)
+  it 'se manda mail dando los parametros' do
     expect(mandador_mail).to receive(:enviar).with('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
+
     mandador_mail.enviar('remitente@test.com', 'destinatario@test.com', 'Prueba', 'Mensaje de prueba')
   end
 
